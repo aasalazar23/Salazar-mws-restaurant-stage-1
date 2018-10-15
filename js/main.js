efed27565157196fe3385f115865f8f73071a3ab
aasalazar23 <aasalazar23@users.noreply.github.com>
2018-10-15 15:03:45 -0600

    Added class names to restaurant items

--- js/main.js
+++ js/main.js
@@ -164,18 +164,22 @@ createRestaurantHTML = (restaurant) => {
   li.append(image);
 
   const name = document.createElement('h1');
+  name.className = 'restaurant-name';
   name.innerHTML = restaurant.name;
   li.append(name);
 
   const neighborhood = document.createElement('p');
+  neighborhood.className = 'restaurant-neighborhood';
   neighborhood.innerHTML = restaurant.neighborhood;
   li.append(neighborhood);
 
   const address = document.createElement('p');
+  address.className = 'restaurant-address';
   address.innerHTML = restaurant.address;
   li.append(address);
 
   const more = document.createElement('a');
+  more.className = 'restaurants-more';
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
