6dd8038c94c6faccb08d6d233e9827d4f3b1dcff
aasalazar23 <aasalazar23@users.noreply.github.com>
2018-10-21 12:16:02 -0600

    centralized sw registration and added js link to each html page

--- js/main.js
+++ js/main.js
@@ -215,15 +215,3 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 
 } 
 
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // successful registration
-      console.log('Service registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // failed registration
-      console.log('Service worker failed: ', err);
-    });
-  });
-}
