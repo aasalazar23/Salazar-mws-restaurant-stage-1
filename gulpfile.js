6f72cbc12015f681fd7728d00a710faaa8807820
aasalazar23 <aasalazar23@users.noreply.github.com>
2018-10-24 13:46:25 -0600

    Added sass and autoprefixer functionality for css

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,20 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
-function defaultTask() {
+gulp.task("default", function() {
     console.log("hello!");
-}
+});
 
-exports.default = defaultTask
+gulp.task("styles", function() {
+    gulp
+        .src("sass/**/*.scss")
+        .pipe(sass())
+        .on("error", sass.logError)
+        .pipe(
+            autoprefixer({
+                browsers: ["last 2 versions"]
+            })
+        )
+        .pipe(gulp.dest("./css"));
+});
