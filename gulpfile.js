f32157a02368138a3b03973c704528101a37b108
aasalazar23 <aasalazar23@users.noreply.github.com>
2018-10-29 10:58:17 -0600

    created css in dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task("styles", function() {
         browsers: ["last 2 versions"]
       })
     )
-    .pipe(gulp.dest("./css"))
+    .pipe(gulp.dest("dist/css"))
     .pipe(browserSync.stream());
 });
 
